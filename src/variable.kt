9295fadceefbbb98e7486d35ec08d006e4e0ad62
sangwook16074 <qazws78941@gmail.com>
2024-04-27 14:31:27 +0900

    variable

---
+++ src/variable.kt
@@ -0,0 +1,13 @@
+fun main() {
+    /*
+    변수는 선언하고 나서 값이 변경될 수 있다.
+     */
+    var number = 20
+    println(number)
+    number = 10
+    println(number)
+    var name = "빵빵이"
+    println(name)
+    name = "옥지"
+    println(name)
+}

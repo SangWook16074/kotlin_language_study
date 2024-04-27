27c2325ea6d13100e48d066c16840e6d1f70eafe
sangwook16074 <qazws78941@gmail.com>
2024-04-27 15:44:13 +0900

    loop

---
+++ src/loop.kt
@@ -0,0 +1,20 @@
+fun main() {
+    // for
+    for (i in 0..10) {
+        println(i)
+    }
+
+    // while
+    var j : Int = 0
+    while(j < 5) {
+        println(j)
+        j++
+    }
+
+    // do ~ while
+    do {
+        println(j)
+        j ++
+    } while (j < 5)
+
+}
